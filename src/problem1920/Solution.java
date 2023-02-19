5deb0e55e9f9d33a23eb95f332080fa26a5f4510
Arthur <arthurleonel7@gmail.com>
2023-02-19 14:56:40 -0300

    resolution of problem 1920 added.

---
+++ src/problem1920/Solution.java
@@ -0,0 +1,12 @@
+package problem1920;
+
+public class Solution {
+    public int[] buildArray(int[] nums) {
+        int ans[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+
