0eaa9aea8a1758b60c4a02839e92f72f279c0e3a
Arthur <arthurleonel7@gmail.com>
2023-02-20 14:10:22 -0300

    Solution of problem 136 added.

---
+++ src/problem136/Solution.java
@@ -0,0 +1,15 @@
+package problem136;
+
+public class Solution {
+
+    public static int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+
+    }
+}
+
+
